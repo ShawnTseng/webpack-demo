dce6d46debc59ec361c83bc12550b6d086ee9f05
ShawnTseng <shawntseng40@gmail.com>
2019-12-11 14:40:04 +0800

    add: packing scss

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 module.exports = {
     entry: ['./utils.js', './src/app.js'],
@@ -17,6 +18,16 @@ module.exports = {
                 test: /\.css$/,
                 exclude: /node_modules/,
                 loader: 'style-loader!css-loader'
+            },
+            {
+                test: /\.scss$/,
+                exclude: /node_modules/,
+                use: [
+                    // 需要用到的 loader
+                    MiniCssExtractPlugin.loader,
+                    "css-loader",
+                    "sass-loader"
+                ]
             }
             // {
             //     test: /\.es6$/,
@@ -29,5 +40,12 @@ module.exports = {
             //     }
             // }
         ]
-    }
+    },
+    plugins: [
+        new MiniCssExtractPlugin({
+            // 指定輸出位置
+            // [name] 為上方進入點設定的 "名稱"
+            filename: "./css/[name].css"
+        })
+    ]
 }
